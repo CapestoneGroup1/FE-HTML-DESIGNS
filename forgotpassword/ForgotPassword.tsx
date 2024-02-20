a57ef0068317a6a347ced3ed2aa4db463a7190de
riyagodhani <158217517+riyagodhani@users.noreply.github.com>
2024-02-20 09:36:13 -0500

    css added

---
+++ forgotpassword/ForgotPassword.tsx
@@ -0,0 +1,139 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import { useForgotPassword, useResetPassword } from "../../api/auth";
+import { useNavigate } from "react-router-dom";
+import { env } from "../../utils/env";
+import { Col, Container, Row, Stack } from "react-bootstrap";
+import "./forgot.css"; // Import the CSS file
+
+export default function ForgotPassword() {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    otp: "",
+    view: "FORGOT_PASSWORD",
+  });
+  const { postData, hasError, error, isLoading, data } = useForgotPassword();
+  const {
+    postData: resetPasswordAPI,
+    hasError: resetError,
+    error: resetErrorData,
+    isLoading: resetLoading,
+    data: resetData,
+  } = useResetPassword();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoading) return;
+    if (!hasError && data?.message) {
+      setState({ ...state, view: "RESET_PASSWORD" });
+    }
+  }, [data, hasError, error, isLoading]);
+
+  useEffect(() => {
+    if (resetLoading) return;
+    if (!resetError && resetData?.message) {
+      alert(resetData?.message);
+      navigate(env.routes.login);
+    }
+  }, [navigate, resetData, resetError, resetErrorData, resetLoading]);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  const submitSendOtp = (e: any) => {
+    if (isLoading) return;
+    e.preventDefault();
+    const { email } = state;
+    postData({
+      email,
+    });
+  };
+
+  const submitResetPassword = (e: any) => {
+    if (resetLoading) return;
+    e.preventDefault();
+    const { email, password, otp } = state;
+    resetPasswordAPI({
+      email,
+      otp,
+      password,
+    });
+  };
+
+  return (
+    <div className="login-container">
+      <Container>
+        <div className="login-form">
+          <Row className="justify-content-center align-items-center">
+            {/* Image column */}
+            <Col xs={12} md={6} className="image-column">
+              <img src="/logo.png" alt="Login" className="img-fluid" />
+            </Col>
+            {/* Login form column */}
+            <Col xs={12} md={6} className="login-form-column">
+              <h1 className="text-center">Forgot Password</h1>
+              {state.view === "FORGOT_PASSWORD" && (
+                <div>
+                  <form onSubmit={submitSendOtp}>
+                    <Stack gap={3}>
+                      {hasError && error?.error && (
+                        <div className="error">
+                          {error?.error || "Internal Server Error!!!"}
+                        </div>
+                      )}
+                      <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        onChange={onChange}
+                        required
+                        className="form-input"
+                      />
+                      <input type="submit" value="Send OTP" className="btn-login" />
+                      <a href={env.routes.login}>Back to Login</a>
+                    </Stack>
+                  </form>
+                </div>
+              )}
+              {state.view === "RESET_PASSWORD" && (
+                <div>
+                  <form onSubmit={submitResetPassword}>
+                    <Stack gap={3}>
+                      {resetError && resetErrorData?.error && (
+                        <div className="error">
+                          {resetErrorData?.error || "Internal Server Error!!!"}
+                        </div>
+                      )}
+                      <input
+                        type="number"
+                        name="otp"
+                        placeholder="OTP"
+                        onChange={onChange}
+                        required
+                        className="form-input"
+                      />
+                      <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        onChange={onChange}
+                        required
+                        className="form-input"
+                      />
+                      <input type="submit" value="ResetPassword" className="btn-login" />
+                    </Stack>
+                  </form>
+                </div>
+              )}
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </div>
+  );
+}
---
+++ signup/SignUp.tsx
@@ -0,0 +1,92 @@
+import { ChangeEvent, useState } from "react";
+import { useLogin } from "../../api/auth";
+import { env } from "../../utils/env";
+import { Col, Container, Row, Stack } from "react-bootstrap";
+import "./signup.css"; // Import the CSS file
+
+export default function Login() {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+  const { postData, hasError, error, isLoading } = useLogin();
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  const submitLoginForm = (e: any) => {
+    if (isLoading) return;
+    e.preventDefault();
+    const { email, password } = state;
+    postData({
+      email,
+      password,
+    });
+  };
+
+  return (
+    <div className="login-container">
+      <Container>
+        <div className="login-form">
+        <Row className="justify-content-center align-items-center">
+          {/* Image column */}
+          <Col xs={12} md={6} className="image-column">
+            <img src="/logo.png" alt="Login" className="img-fluid" />
+          </Col>
+          {/* Login form column */}
+          <Col xs={12} md={6} className="login-form-column">
+            
+              <h1 className="text-center">Signup</h1>
+              <form onSubmit={submitLoginForm}>
+                <Stack gap={3}>
+                  {hasError && error?.error && (
+                    <div className="error">
+                      {error?.error || "Internal Server Error!!!"}
+                    </div>
+                  )}
+                 <input
+                      type="text"
+                      name="username"
+                      placeholder="UserName"
+                      onChange={onChange}
+                      maxLength={10}
+                      required
+                    className="form-input"
+
+                    />
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      onChange={onChange}
+                      required
+                    className="form-input"
+
+                    />
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      onChange={onChange}
+                      maxLength={8}
+                      required
+                    className="form-input"
+                    />
+                    <input type="submit" value="Sign Up" className="btn-signup"/>
+                    <div className="d-flex justify-content-center">
+                      <a href={env.routes.login}>Already have an account</a>
+                    </div>
+                </Stack>
+              </form>
+          </Col>
+        </Row>
+        </div>
+      </Container>
+    </div>
+  );
+}
